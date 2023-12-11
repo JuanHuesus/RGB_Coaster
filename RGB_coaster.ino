2bb057d791bab1e13fdc63ff8795c1e75bcb2afd
vilho m <vilho.makelainen1@gmail.com>
2023-12-11 19:35:54 +0200

    Cleaned up and modified to fill requirements

--- RGB_coaster.ino
+++ RGB_coaster.ino
@@ -1,34 +1,41 @@
 #include <FastLED.h>
-#include <Wire.h>
+#include <avr/wdt.h>
 #include <rgb_lcd.h>
 
 // Define pins for data
-
 // lcd SDA -> A4
 // lcd SCL -> A5
 #define LED_STRIP_PIN    2  
 #define NUM_LEDS         18
-#define LM35_PIN         A6   
+#define BRIGHTNESS       20
+
+#define LM35_PIN         6   
 #define SWITCH_PIN       3
-#define soundSensorPin   A3
+#define SOUND_PIN        3
+
+#define SIMPLE_ANIMATION_SPEED  10
+#define TEMP_UPDATE_INTERVAL    5000
+#define AUDIO_SAMPLE_AMOUNT     10
+
+// Define rgb palettes for the LED strip
+uint8_t paletteIndex = 0;
 
 // initialize the LED strip and LCD screen
 CRGB leds[NUM_LEDS];
 rgb_lcd lcd;
 
-
-// Define rgb palettes for the LED strip
-uint8_t paletteIndex = 0;
-
-DEFINE_GRADIENT_PALETTE (heatmap_gp) {
+// Define palette
+DEFINE_GRADIENT_PALETTE(heatmap_gp) {
   0,   0,   0,   0,   //black
   128, 255,   0,   0,   //red
   200, 255, 255,   0,   //bright yellow
   255, 255, 255, 255    //full white 
 };
 
+// Set palette
 CRGBPalette16 myPal = heatmap_gp;
 
+// Define
 DEFINE_GRADIENT_PALETTE(soundPalette_gp) {
   0,    128, 0, 128,  // Purple
   64,   0,   200, 255,  // Cyan
@@ -36,153 +43,165 @@ DEFINE_GRADIENT_PALETTE(soundPalette_gp) {
   255,  255, 0,   255   // Pink
 };
 
+// Set
 CRGBPalette16 soundPalette = soundPalette_gp;
 
-// variables for the button switch
+// Switch states
 enum AnimationState {
-  TEMPERATURE_MAP,
-  HeatMap,
-  Sounding
+  Basic,
+  Temperature,
+  Sound
 };
 
-// variables for reading the temperature etc
-AnimationState currentAnimationState = TEMPERATURE_MAP;
+// Default led profile
+AnimationState currentAnimationState = Basic;
 
-const unsigned long temperatureUpdateInterval = 1000;
-const unsigned long customAnimationInterval = 5;
-const unsigned long soundReadInterval = 10;
-
-unsigned long lastTemperatureUpdate = 0;
-unsigned long lastCustomAnimationUpdate = 0;
-unsigned long lastSoundReadTime = 0;
-
-unsigned long lastSwitchTime = 0;
-unsigned long switchCooldown = 500;  // Reduce the cooldown time
-
-bool lastSwitchState = HIGH;
-bool switchState = HIGH;
-bool debounceSwitchState();
-
-
-// setup for Serial Monitor, pins and the LED strip
+// setup
 void setup() {
-  // Serial.begin(9600);
+  wdt_disable();  // Disable the watchdog and wait for more than 2 seconds
+  delay(3000);  // Done so that the Arduino doesn't keep resetting infinitely in case of wrong configuration
+  wdt_enable(WDTO_2S);  // Enable the watchdog with a timeout of 2 seconds
 
-  pinMode(SWITCH_PIN, INPUT_PULLUP);
-  pinMode(soundSensorPin, INPUT);
+  // Setup input and pullup for button
+  DDRD &= ~(1 << SWITCH_PIN); // Set as input (clear the bit)
+  PORTD |= (1 << SWITCH_PIN); // Enable pull-up resistor (set the bit)
 
-  lcd.begin(16, 2); 
+  // Configure the ADC
+  // Set the reference voltage to AVCC (5V)
+  ADMUX |= (1 << REFS0);
+  
+  // Set the prescaler to 128 (ADC clock frequency is 16MHz / 128 = 125kHz)
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  
+  // Enable the ADC
+  ADCSRA |= (1 << ADEN);
 
+  lcd.begin(16, 2); // 16x2 lcd
 
   FastLED.addLeds<WS2813, LED_STRIP_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(50);
+  FastLED.setBrightness(BRIGHTNESS); // 0-255
   FastLED.show();  // Initialize all LEDs to 'off'
+
+  // Set up Timer1 to generate an interrupt every 1ms
+  cli(); // Disable interrupts
+  TCCR1A = 0; // Set Timer1 control register A to 0
+  TCCR1B = 0; // Set Timer1 control register B to 0
+  TCNT1 = 0;  // Initialize counter value to 0
+  OCR1A = 15999; // Set the compare value for a 1ms interrupt (assuming a 16MHz clock)
+  TCCR1B |= (1 << WGM12); // Set Timer1 to CTC (Clear Timer on Compare Match) mode
+  TCCR1B |= (1 << CS11) | (1 << CS10); // Set prescaler to 64 for 16MHz clock
+  TIMSK1 |= (1 << OCIE1A); // Enable Timer1 compare match interrupt
+  sei(); // Enable interrupts
 }
 
-// loop through the different profiles with a button switch, print on the LCD and serial for debugging
+// High priority button timer interrupt
+ISR(TIMER1_COMPA_vect) {
+  // Handle button input with debounce
+  static unsigned long lastDebounceTime = 0;
+  static bool lastSwitchState = HIGH;
+  static bool newSwitchState = LOW;
+  int debounceDelay = 50;
+
+  // read the state of the switch into a local variable:
+  int reading = (PIND & (1 << SWITCH_PIN)) ? HIGH : LOW;
+  
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastSwitchState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  // Compare time
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // if the button state has changed:
+    if (reading != newSwitchState) {
+      newSwitchState = reading;
+
+      // only toggle the LED if the new button state is LOW after press
+      if (reading == LOW) {
+
+        // Cycle the animation state
+        currentAnimationState = static_cast<AnimationState>((currentAnimationState + 1) % 3);
+      }
+    }
+
+    // Togle last state for later comparison
+    lastSwitchState = !lastSwitchState;
+  }
+}
+
+// loop through the different profiles with a button switch, print on the LCD
 void loop() {
   unsigned long currentMillis = millis();
-  int soundLevel = analogRead(soundSensorPin);
-
-  // Read the switch state with debounce
-  switchState = debounceSwitchState();
-
+  
   // Update animation based on the current state
   switch (currentAnimationState) {
-    case TEMPERATURE_MAP:
-      if (currentMillis - lastTemperatureUpdate >= temperatureUpdateInterval) {
-        updateTemperatureMap();
-        lastTemperatureUpdate = currentMillis;
-      }
+    case Basic:
+      updateSimple();
       break;
 
-    case HeatMap:
-      if (currentMillis - lastCustomAnimationUpdate >= customAnimationInterval) {
-        updateCustomAnimation();
-        lastCustomAnimationUpdate = currentMillis;
-      }
+    case Temperature:
+      updateTemperature();
       break;
 
-    case Sounding:
-      if (currentMillis - lastSoundReadTime >= soundReadInterval) {
-        updateSounds();
-        lastSoundReadTime = currentMillis;
-      }
+    case Sound:
+      updateSound();
       break;
   }
-
-  // Check for switch press and update state
-  handleSwitchPress();
-
-  // Display information in the Serial Monitor at a slower rate
- // if (currentMillis % 500 == 0) {
-   //  Serial.print("Temperature: ");
-   //  Serial.print(getTemperature());
-   //  Serial.print(" C | Button State: ");
-   //  Serial.println(switchState);
- // }
+   // Reset watchdog
+  wdt_reset();
 }
 
-void handleSwitchPress() {
-  static bool buttonWasPressed = false;
-  if (switchState == LOW && !buttonWasPressed) {
-    toggleLightMode();
-    buttonWasPressed = true;
-  } else if (switchState == HIGH) {
-    buttonWasPressed = false;
-  }
-}
-
-void updateTemperatureMap() {
-
-  float temp_val = getTemperature();
- //  Serial.print("Temperature = ");
- //  Serial.print(temp_val);
- //  Serial.println(" Degree Celsius");
-
-  // Display temperature on the LCD
-  lcd.setCursor(0, 0);
-  lcd.print("Temp: ");
-  lcd.print(temp_val);
-  lcd.print(" C  ");
-  lcd.setCursor(0, 1);
-  lcd.print("Lighting: Temp");
-
-  // Map the temperature range to the color range
-  CRGB color = mapTemperatureToColor(temp_val);
-
-  // Set the color of the entire LED strip
-  setColor(color);
-}
-
-  // LED animation logic for the HeatMap profile, which just rotates a palette around the selected LEDs
-void updateCustomAnimation() {
+// LED animation logic for the simple profile, which just rotates a palette around the selected LEDs
+void updateSimple() {
   fill_palette(leds, NUM_LEDS, paletteIndex, 255 / NUM_LEDS, myPal, 255, LINEARBLEND);
 
-  EVERY_N_MILLISECONDS(1) {
+  EVERY_N_MILLISECONDS(SIMPLE_ANIMATION_SPEED) {
     paletteIndex++;
   }
+  
   // LCD code to show what lighting profile is applied
   FastLED.show();
   lcd.setCursor(0, 0);
-  lcd.print("Lighting:            ");
+  lcd.print("                ");
   lcd.setCursor(0, 1);
-  lcd.print("Heat Map             ");
+  lcd.print("Lighting: Simple");
 }
 
-  // code for getting the temperature and converting it to Celsius
+void updateTemperature() {
+  float temp = getTemperature();
+  
+  // Display temperature on the LCD
+  lcd.setCursor(0, 0);
+  lcd.print("Temp: ");
+  lcd.print(temp);
+  lcd.print(" C   ");
+  lcd.setCursor(0, 1);
+  lcd.print("Lighting: Temp  ");
+
+  // Map the temperature range to the color range
+  CRGB color = mapTemperatureToColor(temp);
+
+  // Set the color of the entire LED strip
+  fill_solid(leds, NUM_LEDS, color);
+  FastLED.show();
+}
+
+// code for getting the temperature and converting it to Celsius
 float getTemperature() {
-
-  static unsigned long lastTemperatureReadTime = 50;
-  static const unsigned long temperatureReadInterval = 500;
-
+  static unsigned long lastTemperatureReadTime = 0;
   unsigned long currentMillis = millis();
-  if (currentMillis - lastTemperatureReadTime >= temperatureReadInterval) {
-    lastTemperatureReadTime = currentMillis;
-  }
+  int tempADC;
 
-  int temp_adc_val = analogRead(LM35_PIN);
-  return (temp_adc_val * 4.88) / 10.0;  // Convert adc value to temperature in Celsius
+  // Compare time
+  if (currentMillis - lastTemperatureReadTime >= TEMP_UPDATE_INTERVAL) {
+    lastTemperatureReadTime = currentMillis;
+    // Get temperature reading
+    tempADC = readADC(LM35_PIN);
+    // Convert adc value to temperature in Celsius
+    tempADC = (tempADC / 2.048) - 6; // Calibration overshoots about 6c
+  }
+  return tempADC;
 }
 
 // color coding for the LED profile which reacts to the temperature
@@ -192,47 +211,29 @@ CRGB mapTemperatureToColor(float temperature) {
   return CRGB(r, g, 0);
 }
 
-  // sets the color evenly across the appointed LEDs
-void setColor(CRGB color) {
-  fill_solid(leds, NUM_LEDS, color);
-  FastLED.show();
-}
-
-   
-void toggleLightMode() {
-  // Toggle the animation state
-  currentAnimationState = static_cast<AnimationState>((currentAnimationState + 1) % 3);
-}
-
-  // Code for processing the detected sound with smoothing
-void updateSounds() {
-  const int numReadings = 10;
-
-  int soundReadings[numReadings];
-
+// Code for processing the detected sound with smoothing
+void updateSound() {
   int soundIndex = 0;
   int totalSound = 0;
+  int soundReadings[AUDIO_SAMPLE_AMOUNT];
 
-  int soundLevel = analogRead(soundSensorPin);
-
+  // Get sound reading
+  int soundLevel = readADC(SOUND_PIN);
+  
   // Smoothing
   totalSound = totalSound - soundReadings[soundIndex] + soundLevel;
   soundReadings[soundIndex] = soundLevel;
-  soundIndex = (soundIndex + 1) % numReadings;
+  soundIndex = (soundIndex + 1) % AUDIO_SAMPLE_AMOUNT;
 
-  int averageSound = totalSound / numReadings;
-  
-  // Debug
- //  Serial.print("Sound level: ");
- // Serial.println(averageSound);
+  int averageSound = totalSound / AUDIO_SAMPLE_AMOUNT;
 
   // LCD for the sound profile
   lcd.setCursor(0, 0);
   lcd.print("Sound level: ");
-  lcd.setCursor(0, 1);
   lcd.print(averageSound);
-  lcd.setCursor(2, 1);
-  lcd.print("                "); // Clear the line
+  lcd.print(" ");
+  lcd.setCursor(0, 1);
+  lcd.print("Lighting: Sound ");
 
   // Smoothed LED brightness
   int brightness = map(averageSound, 0, 100, 0, 255);
@@ -240,7 +241,7 @@ void updateSounds() {
   CRGB color;
 
   // Logic for going between colors while reactive to sound
-  int soundThreshold = 10;
+  int soundThreshold = 2;
   if (averageSound > soundThreshold) {
   // If the sound level exceeds the threshold, smoothly transition to a different color
     color = CRGB(0, brightness, 0);
@@ -254,24 +255,16 @@ void updateSounds() {
   FastLED.show();
 }
 
-  // debounce for the button
-bool debounceSwitchState() {
-  static unsigned long lastDebounceTime = 0;
-  static unsigned long debounceDelay = 50;
 
-  bool reading = digitalRead(SWITCH_PIN);
+int readADC(int pin){
+  // Set ADC channel
+  ADMUX &= ~((1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (1 << MUX0));
+  ADMUX |= pin;
 
-  if (reading != lastSwitchState) {
-    lastDebounceTime = millis();
-  }
+  // Start ADC conversion
+  ADCSRA |= (1 << ADSC);
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != switchState) {
-      switchState = reading;
-    }
-  }
-
-  lastSwitchState = reading;
-  return switchState;
+  // Wait for conversion to complete
+  while (ADCSRA & (1 << ADSC));
+  return ADC;
 }
-
